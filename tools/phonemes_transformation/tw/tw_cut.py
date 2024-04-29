b1bf433002e319f612b27827cf95a48549f830d5
Paintakotako <p76111652@gs.ndku.edu.tw>
2024-04-29 15:18:17 +0800

    fix: Fix phoneme segmentation
    Before fixed, the vowel will not follow the correct sequence.
    For example:
    Input: manni2 ti3 au3 piah4 koong1 dlang5 e7 phainn1 ue7
    Correct output shold be:
    m0 anni02 t0 i03 au03 p0 iah04 k0 oong01 dl0 ang05 e07
    ph0 ainn01 ue07
    
    But the wrong _get_initials_finals functinos gives the following
    result:
    m0  anni02  t0  i03  iah04  p0  oong01  k0  ang05  dl0  ainn01
    
    This error output leads to misalignment during training, so it
    needs to be fixed.

--- tools/phonemes_transformation/tw/tw_cut.py
+++ tools/phonemes_transformation/tw/tw_cut.py
@@ -14,14 +14,13 @@ class tw_frontend():
         orig_initials, orig_finals = self._cut_vowel(sentence)
         for c, v in zip(orig_initials, orig_finals):
             if c and c not in self.punc:
-                initials.append(f'{c}0 ')
+                initials.append(c+'0')
             else:
                 initials.append(c)
-                continue
-
-            finals.append(f'{v[:-1]}0{v[-1]} ')
-            
-            
+            if v not in self.punc:
+                finals.append(v[:-1]+'0'+v[-1])
+            else:
+                finals.append(v)
     
         return initials, finals, True
 
@@ -73,13 +72,10 @@ class tw_frontend():
         phonemes, status = self._g2p(sentence)
         if status == False:
             return [], False
+        print(phonemes)
         r = ''
         for p in phonemes:
-            if not p.endswith('-'):
-                r = f'{r}{p}'
-            else:
-                r = f'{r} {p}'
-        r = r.strip()
+            r = f'{r}{p} '
         
         return r, True
     
@@ -90,7 +86,8 @@ if __name__ == "__main__":
         "gua1 i1 kieng1 sciunn3 bo7 kh ah1 h o2 e7 ke3 ti3 ah3",
         "tscit8 kieng7 tiam2 sci3 sng3 dlang7 thau5 sciu7 tscinn5 e7",
         "dli1 e7 pak8 too2 nann1 e3 hia2 ninn7 tua3 khieen1",
-        "gua1 tsu3 thau5 tioh8 bo7 scioong7 scin3 i7 e7 ue7"
+        "gua1 tsu3 thau5 tioh8 bo7 scioong7 scin3 i7 e7 ue7",
+        "manni2 ti3 au3 piah4 koong1 dlang5 e7 phainn1 ue7"
 
     ]
     for t in text:
